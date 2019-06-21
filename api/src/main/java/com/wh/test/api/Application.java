4d1b34ea08dc180f1e0ceae5a7bdd33399f49e0c
wanghuan <wanghuan@ele-cloud.com>
2019-06-21 16:08:48 +0800

    github   学习提交

--- api/src/main/java/com/wh/test/api/Application.java
+++ api/src/main/java/com/wh/test/api/Application.java
@@ -8,6 +8,16 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({CoreConfiguration.class, DataConfiguration.class})
+/****
+ *
+ * 功能描述: 
+ *
+ * @auther: wh
+ * 
+ * @param null
+ * @date: 2019/6/21 16:08
+ */
+
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
